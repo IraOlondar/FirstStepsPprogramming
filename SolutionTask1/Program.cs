8ea348eb1bba5f70ba75971a9d35fb726b0f908d
Olondar <olondar@yandex.ru>
2022-07-08 22:37:43 +0300

    finished SolutionTask1

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+* Проверка на равенство второго числа квадрату первого
+*/
+Console.WriteLine("введите первое число:");
+string? firstNumber = Console.ReadLine();
+Console.WriteLine("введите второе число:");
+string? secondNumber = Console.ReadLine();
+
+if (firstNumber != null && secondNumber != null) {
+    int firstOutNumber = int.Parse(firstNumber);
+    int secondOutNumber = int.Parse(secondNumber);
+    int firstOutQuatroNumber = (int) Math.Pow(firstOutNumber,2);
+    
+    if (firstOutQuatroNumber == secondOutNumber) {
+        Console.WriteLine("да");
+    } else {
+        Console.WriteLine("нет");
+    }
+}
