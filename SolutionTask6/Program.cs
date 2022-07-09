e2b008c238e2f3611927a643e3bce2cc9467b44e
olondar <olondar@yandex.ru>
2022-07-09 20:04:02 +0300

    add SolutionTask6

---
+++ SolutionTask6/Program.cs
@@ -0,0 +1,15 @@
+﻿/**
+* Проверка на четность
+*
+*/
+Console.WriteLine("веедите число:");
+string? number = Console.ReadLine();
+
+if (number != null) {
+    int outNumber = int.Parse(number);  
+     if (outNumber % 2 > 0) {
+        Console.WriteLine("не четное");
+    } else {
+        Console.WriteLine("четное");
+    }
+}
