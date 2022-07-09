1b8c981faa573fe6336b255a05c3237ff9941c0d
olondar <olondar@yandex.ru>
2022-07-09 19:54:07 +0300

    add SolutionTask5

---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+* Вывод всех целых чисел в промежутке от -N до N
+*
+*/
+Console.WriteLine("веедите число:");
+string? number = Console.ReadLine();
+
+if (number != null) {
+    int outNumber = int.Parse(number);  
+    int startNomber = outNumber * -1;
+    string text = "";
+    while (startNomber < outNumber) {
+        text += startNomber + ", ";
+        startNomber++;
+    }
+
+    text += outNumber;
+    Console.WriteLine(text);
+}
