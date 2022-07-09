f510a69c68731bb0ba3b39baf1fd9784b0700a7c
olondar <olondar@yandex.ru>
2022-07-09 19:07:12 +0300

    add SolutionTask4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,18 @@
+﻿/**
+* Молучение максимального числа из введенных
+*
+*/
+Console.WriteLine("вводите последовательно числа через пробел:");
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+if (arr.Length != 0) {
+    int max = arr[0];
+    foreach (int num in arr) {
+        if (max < num) {
+            max = num;
+        }
+    }
+    string maxText = "max: ";
+    maxText += max; 
+    Console.WriteLine(maxText);
+}
