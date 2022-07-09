cb06ef65d20407a2b4d67e3f579ff3af8234abca
olondar <olondar@yandex.ru>
2022-07-09 17:40:26 +0300

    finished SolutionTask2

---
+++ SolutionTask0/Program.cs
@@ -0,0 +1,14 @@
+﻿/**
+* Вычисление квадрата введенного числа
+*
+*//*  */
+Console.WriteLine("введите число");
+string? inputInline = Console.ReadLine();
+
+if (inputInline != null) {
+    int inputNumber = int.Parse(inputInline);
+    //int outNumber = inputNumber*inputNumber;
+    int outNumber = (int) Math.Pow(inputNumber,2);
+    string text = "квадрат числа " + inputNumber + " будет: " + outNumber;
+    Console.WriteLine(text);
+}
---
+++ SolutionTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+* Проверка на равенство второго числа квадрату первого
+*
+*/
+Console.WriteLine("введите первое число:");
+Console.WriteLine("введите второе число:");
+
+string? firstNumber = Console.ReadLine();
+string? secondNumber = Console.ReadLine();
+
+if (firstNumber != null && secondNumber != null) {
+    int firstOutNumber = int.Parse(firstNumber);
+    int secondOutNumber = int.Parse(secondNumber);
+    string max = "max: ";
+    string min = "min: ";
+    if (firstOutNumber > secondOutNumber) {
+        max += firstOutNumber;
+        min += secondOutNumber;
+    } else {
+        max += firstOutNumber;
+        min += secondOutNumber;
+    }
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+}
