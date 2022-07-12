4a2001651be8ca4362a208f02e86676cfa0a43b7
Olondar <olondar@yandex.ru>
2022-07-13 01:58:21 +0300

    add SolutionTask12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+* Проверка на кратность второго числа первому
+*
+*/
+Console.WriteLine("введите первое число:");
+string firstNumber = Console.ReadLine() ?? "";
+Console.WriteLine("введите второе число:");
+string secondNumber = Console.ReadLine() ?? "";
+
+void funck (int a, int b) {
+    string text = "";
+    if (a != 0 && b != 0) {
+        int c = b % a;     
+        if (c == 0) {
+            text = "да, второе число кратно первому"; 
+        } else {
+            text = "нет, второе число не кратно первому, остаток: " + c; 
+        }
+        Console.WriteLine(text);
+    } else {
+        text = "ошибка ввода(a: " + a + ", b: " + b + ")";
+        Console.WriteLine(text);
+    }
+}
+
+if (firstNumber != "" && secondNumber != "") {
+    int firstOutNumber = int.Parse(firstNumber);
+    int secondOutNumber = int.Parse(secondNumber);
+    funck(firstOutNumber, secondOutNumber);
+
+}
