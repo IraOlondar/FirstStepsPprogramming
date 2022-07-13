8b6652d181e5b45e60ca3ae38ac0e321a71acf9c
Olondar <olondar@yandex.ru>
2022-07-13 21:48:35 +0300

    add folder SolutionTask9

---
+++ SolutionTask9/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+* Наибольшая цифра сдучайного числа между 10 и 99
+* 
+*/
+int number = new Random().Next(10,99);
+Console.WriteLine(number);
+
+string? text = number.ToString();
+
+int a = int.Parse(text.Substring(0, 1));
+int b = int.Parse(text.Substring(1, 1));
+int max = 0;
+ if (a > b) {
+    max = a;
+ } else {
+    max = b;
+ }
+
+ Console.WriteLine(max);
