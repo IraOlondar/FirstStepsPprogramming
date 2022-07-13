c7831c76799d9948f7bb279914149eedd95ddf26
Olondar <olondar@yandex.ru>
2022-07-13 23:52:23 +0300

    add edits SolutionTask13

--- SolutionTask13/Program.cs
+++ SolutionTask13/Program.cs
@@ -3,41 +3,39 @@
 *
 */
 //Метод №1
-// int n = 3; //порядковый номер нужной нам цифры для вывода
-// Console.WriteLine("введите число:");
-// string? str = Console.ReadLine() ?? "";
-// string text = "";
-// try {
-//     double ss = Char.GetNumericValue (str, (n-1));
-//     if (ss != -1) {
-//         text = "цифра с порядковым номером " + n + " равна: " + ss;
-//     }
-// } catch (ArgumentOutOfRangeException) {
-//     text = "цифры с порядковым номером " + n + " в веденном числе нет!";
-// }
+int n = 3; //порядковый номер нужной нам цифры для вывода
+Console.WriteLine("введите число:");
+
+string text = "";
+try {
+    int? ss = Convert.ToInt32(Char.GetNumericValue (Console.ReadLine() ?? "", (n-1)));
+    if (ss != -1) {
+        text = "цифра с порядковым номером " + n + " равна: " + ss;
+    }
+} catch (ArgumentOutOfRangeException) {
+    text = "цифры с порядковым номером " + n + " в веденном числе нет!";
+}
 
 Console.WriteLine(text);
 
 //Метод №2
-int n = 3; //порядковый номер нужной нам цифры для вывода
-Console.WriteLine("введите число:");
-string? str = Console.ReadLine() ?? "";
-string text = "";
-int[] arr = str.Select(x => x - '0').ToArray();
-if (arr.Length >= n) {
-    text = "цифра с порядковым номером " + n + " равна: " + arr[n-1];
-} else {
-    text = "цифры с порядковым номером " + n + " в веденном числе нет!";
-}
-Console.WriteLine(text);
+// int n = 3; //порядковый номер нужной нам цифры для вывода
+// Console.WriteLine("введите число:");
+// int[]? arr = Console.ReadLine()?.Select(x => x - '0').ToArray();
+// string text = "";
+// if (arr?.Length >= n) {
+//     text = "цифра с порядковым номером " + n + " равна: " + arr[n-1];
+// } else {
+//     text = "цифры с порядковым номером " + n + " в веденном числе нет!";
+// }
+// Console.WriteLine(text);
 
 //Метод №3
 // int n = 3; //порядковый номер нужной нам цифры для вывода
 // Console.WriteLine("введите число:");
-// string? str = Console.ReadLine() ?? "";
 // string text = "";
-// int[] arr = str.Select(x => Convert.ToInt32(Char.GetNumericValue(x))).ToArray();
-// if (arr.Length >= n) {
+// int[]? arr = Console.ReadLine()?.Select(x => Convert.ToInt32(Char.GetNumericValue(x))).ToArray();
+// if (arr?.Length >= n) {
 //     text = "цифра с порядковым номером " + n + " равна: " + arr[n-1];
 // } else {
 //     text = "цифры с порядковым номером " + n + " в веденном числе нет!";
