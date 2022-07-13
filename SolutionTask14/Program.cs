94bf4d341cc19637752385eafcc72af42d167380
Olondar <olondar@yandex.ru>
2022-07-13 22:22:21 +0300

    add SolutionTask14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,27 @@
+﻿/**
+* Проверка на кратность 7 и 23 одновременно, введенного числа
+* 
+*/
+Console.WriteLine("введите число:");
+string? number = Console.ReadLine();
+int b = 7;
+int c = 23;
+int outNumber = int.Parse(number);
+
+void fun (int number, int b, int c) {
+    if (number != null && b != null && c != null) {
+        int is_null_b = outNumber  % b;
+        int is_null_c = outNumber  % c;
+        string iext = "";
+        
+        if (is_null_b == 0 && is_null_c == 0) {
+            iext = "да"; 
+            Console.WriteLine(iext);
+        } else {
+            iext = "нет"; 
+            Console.WriteLine(iext);
+        }
+    }
+}
+
+fun(outNumber, b, c);
