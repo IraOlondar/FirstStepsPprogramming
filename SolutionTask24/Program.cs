cf0613d00e32241c50f396ad3e096054b0283cf7
Olondar <olondar@yandex.ru>
2022-08-03 14:25:45 +0300

    add SolutionTask24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+* Сумма чисел от 1 до A
+*
+*/
+
+void variantSimple (int num) {
+    int i = 1;
+    int sum = 0;
+
+    while (i <= num) {
+        sum += i++;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {num} равна {sum}");
+}
+
+void variantGauss (int num) {
+    int sum = 0;
+
+    //Находим сумму с помощью арифметической прогресии
+    sum  = num * (1 + num) / 2 ;
+    
+    Console.WriteLine($"Сумма чисел от 1 до {num} равна {sum}");
+}
+
+Console.Write("Введите число: ");
+string input = Console.ReadLine() ?? "";
+
+if (input != "") {
+    int t;
+    int inputNumber = int.Parse(input);
+
+    t = Environment.TickCount;
+    variantSimple(inputNumber);
+    Console.WriteLine("Simpli Line: {0} ms", Environment.TickCount - t);
+    
+    variantGauss(inputNumber);
+    Console.WriteLine("Simpli Line: {0} ms", Environment.TickCount - t);
+} else {
+    Console.WriteLine("Ошибка ввода, пустое значение");
+}
