0fea4a625dd5012b4c08a2c79bd5e9bca3ad7076
Olondar <olondar@yandex.ru>
2022-08-04 21:49:24 +0300

    add SolutionTask29_beer

---
+++ SolutionTask29_beer/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+* Кто пойдет за пивом?
+*/
+
+System.Random numberSintezator = new Random();
+
+//Генератор имени
+void genName (string n) {
+    //Разбираем имена на элементы списка
+    string[] nams = n.Replace(" ", "").Split(',').ToArray();
+    
+    //Выводим случайное имя из списка
+    Console.WriteLine($"За пивом пойдет {nams[numberSintezator.Next(0, nams.Length + 1)]}");
+}
+
+Console.WriteLine("Кто пойдет за пивом?");
+Console.WriteLine("Введите через запятую всех учасников вечеринки: ");
+string input = Console.ReadLine() ?? "";
+
+if (input != "") {
+    genName(input); 
+} else {
+    Console.WriteLine("Ошибка ввода, пустое значение");
+}
