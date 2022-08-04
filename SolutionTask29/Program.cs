eb350e98cb90b1aa4b18af058ffbe98c1601a5c9
Olondar <olondar@yandex.ru>
2022-08-04 21:23:13 +0300

    add SolutionTask29

---
+++ SolutionTask29/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+* Вывод массива по заданной длинне и границам диапазона хзначений
+*/
+
+System.Random numberSintezator = new Random();
+
+void genArray (int n, int min, int max) {
+    int i = 0;
+    
+    Console.Write("[");
+    while (i++ < n) {
+        Console.Write(numberSintezator.Next(min, max + 1) + (i == n ? "" : ","));
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.Write("Введите длину массива: ");
+string input = Console.ReadLine() ?? "";
+
+Console.Write("Введите нижнюю границу диапазона значений: ");
+string minValue = Console.ReadLine() ?? "";
+
+Console.Write("Введите верхнюю границу диапазона значений: ");
+string maxValue = Console.ReadLine() ?? "";
+
+if (input != "" && minValue != "" && maxValue != "") {
+    int inputNumber = int.Parse(input);
+    int minValueNumber = int.Parse(minValue);
+    int maxValueNumber = int.Parse(maxValue);
+
+    genArray(inputNumber, minValueNumber, maxValueNumber); 
+} else {
+    Console.WriteLine("Ошибка ввода, пустое значение");
+}
