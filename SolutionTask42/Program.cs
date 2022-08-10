b99b7acb713e7ddbb150913f3af5af9e4ed77e67
Olondar <olondar@yandex.ru>
2022-08-10 19:38:10 +0300

    add SolutionTask42

---
+++ SolutionTask42/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+* Преобразование десятичного числа в двоичное
+*
+*/
+
+//Читаем консоль
+int ReadSieds () {
+    Console.Write("Введите число: ");
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+//Преобразуем десятичное число в двоичное
+string ConvertIntToBin (int n) {
+    return Convert.ToString(n, 2);
+}
+
+//Выводим результат
+void Print (string num) {
+    Console.WriteLine(num);
+}
+
+Print(ConvertIntToBin(ReadSieds()));
