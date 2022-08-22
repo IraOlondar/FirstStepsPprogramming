3afa045001039a559e195bb61e344931b5de4f3b
Olondar <olondar@yandex.ru>
2022-08-22 19:45:56 +0300

    add SolutionTask65

---
+++ SolutionTask65/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+* Задайте значения M и N. Напишите программу, 
+* которая выведет все натуральные числа в промежутке от M до N.
+*
+*/
+
+int Read (string m) {
+    Console.Write(m);
+    string? input = Console.ReadLine() ?? "";
+    return int.Parse(input);
+}
+
+void RecursePrint (int n, int m) {
+    if (n <= m) {
+        Console.Write(n + " ");
+        RecursePrint(++n, m);
+    }
+}
+
+
+int N = Read("Введите N: ");
+int M = Read("Введите M: ");
+
+RecursePrint(N, M);
