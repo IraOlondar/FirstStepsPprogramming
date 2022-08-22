ded4271c61bc05e765c998a9130150609aeef136
Olondar <olondar@yandex.ru>
2022-08-22 20:25:44 +0300

    add SolutionTask66

---
+++ SolutionTask66/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+* Задайте значения N и M. Напишите программу, 
+* которая найдёт сумму натуральных элементов в промежутке от N до M.
+*
+*/
+
+int Read (string m) {
+    Console.Write(m);
+    string? input = Console.ReadLine() ?? "";
+    return int.Parse(input);
+}
+
+int RecurseSumm (int v, int p) {
+    if (p == 1) return v;
+    return v++ + RecurseSumm(v, --p);
+}
+
+int N = Read("Введите N: ");
+int M = Read("Введите M: ");
+
+Console.WriteLine(RecurseSumm(N, M));
