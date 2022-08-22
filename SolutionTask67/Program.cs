beaf624315e52354150ef935b319c78a30f04340
Olondar <olondar@yandex.ru>
2022-08-22 20:00:58 +0300

    add SolutionTask67

---
+++ SolutionTask67/Program.cs
@@ -0,0 +1,20 @@
+﻿/**
+* Задайте значения M и N. Напишите программу, 
+* которая будет принимать на вход число и возвращать сумму его цифр.
+*
+*/
+
+int Read (string m) {
+    Console.Write(m);
+    string? input = Console.ReadLine() ?? "";
+    return int.Parse(input);
+}
+
+int Summ (int n) {
+    if (n == 0) return 0;
+    return n % 10 + Summ(n / 10);
+}
+
+int N = Read("Введите число: ");
+
+Console.WriteLine(Summ(N));
