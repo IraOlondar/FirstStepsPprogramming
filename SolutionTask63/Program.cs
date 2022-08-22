573b8cd9f8eadd57ef48bf3ab5b23f3b90629217
Olondar <olondar@yandex.ru>
2022-08-22 19:35:49 +0300

    add SolutionTask63

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,23 @@
+﻿int Read () {
+    Console.Write("Ведите число: ");
+    string? input = Console.ReadLine() ?? "";
+    if (input != "") {
+        return int.Parse(input);
+    } else {
+        Console.WriteLine("Пустая строка!");
+    }
+    return 0;
+}
+
+void RecursePrint (int n) {
+    if (n >= 1) {
+        Console.Write(n + " ");
+        RecursePrint(--n);
+    }
+}
+
+
+int inputNumber = Read();
+
+RecursePrint(inputNumber);
+
